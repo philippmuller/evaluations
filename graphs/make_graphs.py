58020b37d7b3cdbfdbc51e6e49e06422c62daf0f
Philipp Müller <mailphilippmuller@gmail.com>
2015-08-26 20:47:16 +0200

    removed zero percent labels. include all possible choices in the footer

--- graphs/make_graphs.py
+++ graphs/make_graphs.py
@@ -3,13 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pylab
 
+
+# Some help for coloring
+slices = [1,2,3] * 4 + [20, 25, 30] * 2
+cmap = plt.cm.prism
+colors = cmap(np.linspace(0., 1., len(slices)))
+
 # Overall Course Rating
-x_list_overall = [9, 2, 0, 0, 0]
+x_list_overall = [9, 2]
 label_list_overall = ["Excellent", "Very Good", "Good", "Fair", "Poor"]
 plt.axis("equal")
 plt.pie(
         x_list_overall,
-        autopct="%1.1f%%"
+        autopct="%1.1f%%",
+        colors=colors
         )
 
 plt.legend(label_list_overall, loc='center left', bbox_to_anchor=(0.9, 0.5))
@@ -24,7 +31,8 @@ label_list_university = ["Department of Economics", "Harris School of Public Pol
 plt.axis("equal")
 plt.pie(
         x_list_university,
-        autopct="%1.1f%%"
+        autopct="%1.1f%%",
+        colors=colors
         )
 plt.legend(label_list_university, loc='center left', bbox_to_anchor=(0.9, 0.5))
 plt.savefig("graphs/part_univ.png", bbox_inches='tight')
@@ -34,12 +42,13 @@ plt.close()
 
 
 # Usefulness
-x_list_rating_overall = [0, 0, 1, 2, 7]
-label_list_rating_overall = ["Not useful at all: 1", "2", "3", "4", "Very useful: 5"]
+x_list_rating_overall = [1, 2, 7]
+label_list_rating_overall = ["3", "4", "Very useful: 5"]
 plt.axis("equal")
 plt.pie(
         x_list_rating_overall,
-        autopct="%1.1f%%"
+        autopct="%1.1f%%",
+        colors=colors
         )
 
 plt.legend(label_list_rating_overall, loc='center left', bbox_to_anchor=(0.9, 0.5))
@@ -48,12 +57,13 @@ plt.close()
 ###########
 
 # Well organized?
-x_list_organized = [9,1,0,0,0]
+x_list_organized = [9,1]
 label_list_organized = ["Excellent", "Very Good", "Good", "Fair", "Poor"]
 plt.axis("equal")
 plt.pie(
         x_list_organized,
-        autopct="%1.1f%%"
+        autopct="%1.1f%%",
+        colors=colors
         )
 
 plt.legend(label_list_organized, loc='center left', bbox_to_anchor=(0.9, 0.5))
@@ -62,12 +72,13 @@ plt.close()
 ###########
 
 # Right Speed
-x_list_speed = [8,2,0,0,0]
+x_list_speed = [8,2]
 label_list_speed = ["Excellent", "Very Good", "Good", "Fair", "Poor"]
 plt.axis("equal")
 plt.pie(
         x_list_speed,
-        autopct="%1.1f%%"
+        autopct="%1.1f%%",
+        colors=colors
         )
 
 plt.legend(label_list_speed, loc='center left', bbox_to_anchor=(0.9, 0.5))
@@ -76,12 +87,13 @@ plt.close()
 ###########
 
 # clearly
-x_list_clearly = [10,0,0,0,0]
+x_list_clearly = [10]
 label_list_clearly = ["Excellent", "Very Good", "Good", "Fair", "Poor"]
 plt.axis("equal")
 plt.pie(
         x_list_clearly,
-        autopct="%1.1f%%"
+        autopct="%1.1f%%",
+        colors=colors
         )
 
 plt.legend(label_list_clearly, loc='center left', bbox_to_anchor=(0.9, 0.5))
@@ -90,12 +102,13 @@ plt.close()
 ###########
 
 # accessible
-x_list_access = [9,1,0,0,0]
-label_list_access = ["Excellent", "Very Good", "Good", "Fair", "Poor"]
+x_list_access = [9,1]
+label_list_access = ["Excellent", "Very Good"]
 plt.axis("equal")
 plt.pie(
         x_list_access,
-        autopct="%1.1f%%"
+        autopct="%1.1f%%",
+        colors=colors
         )
 
 plt.legend(label_list_access, loc='center left', bbox_to_anchor=(0.9, 0.5))
