c28fa358b8bce6a341718d4aaa63a76e7312f019
Philipp Müller <mailphilippmuller@gmail.com>
2015-09-03 18:51:34 +0200

    Added the bootcamp evaluations

--- graphs/make_graphs.py
+++ graphs/make_graphs.py
@@ -9,6 +9,9 @@ slices = [1,2,3] * 4 + [20, 25, 30] * 2
 cmap = plt.cm.prism
 colors = cmap(np.linspace(0., 1., len(slices)))
 
+# Course graphs
+###############
+
 # Overall Course Rating
 values = np.array([9, 2, 0, 0, 0])
 labels = np.array(["Excellent", "Very Good", "Good", "Fair", "Poor"])
@@ -29,13 +32,12 @@ for elem in bla[2:]:
 
 ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
 
-fig.savefig("graphs/overall.png", bbox_inches='tight')
+fig.savefig("graphs/course/overall.png", bbox_inches='tight')
 plt.close()
 ###########
 
 
 # University Part
-# !!!!!!!!!!!!!!!!!!!!!!!!Check ordering
 x_list_university = [3, 4, 2, 1]
 label_list_university = ["Department of Economics", "Harris School of Public Policy", "Booth School of Business", "Other"]
 plt.axis("equal")
@@ -45,7 +47,7 @@ plt.pie(
         colors=colors
         )
 plt.legend(label_list_university, loc='center left', bbox_to_anchor=(0.9, 0.5))
-plt.savefig("graphs/part_univ.png", bbox_inches='tight')
+plt.savefig("graphs/course/part_univ.png", bbox_inches='tight')
 plt.close()
 ###########
 
@@ -71,7 +73,7 @@ for elem in bla[:2]:
 
 ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
 
-fig.savefig("graphs/useful.png", bbox_inches='tight')
+fig.savefig("graphs/course/useful.png", bbox_inches='tight')
 plt.close()
 ###########
 
@@ -94,7 +96,7 @@ for elem in bla[2:]:
 
 ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
 
-fig.savefig("graphs/organized.png", bbox_inches='tight')
+fig.savefig("graphs/course/organized.png", bbox_inches='tight')
 plt.close()
 ###########
 
@@ -117,7 +119,7 @@ for elem in bla[2:]:
 
 ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
 
-fig.savefig("graphs/speed.png", bbox_inches='tight')
+fig.savefig("graphs/course/speed.png", bbox_inches='tight')
 plt.close()
 ###########
 
@@ -140,7 +142,7 @@ for elem in bla[1:]:
 
 ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
 
-fig.savefig("graphs/clearly.png", bbox_inches='tight')
+fig.savefig("graphs/course/clearly.png", bbox_inches='tight')
 plt.close()
 ###########
 
@@ -165,6 +167,162 @@ for elem in bla[2:]:
 
 ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
 
-fig.savefig("graphs/access.png", bbox_inches='tight')
+fig.savefig("graphs/course/access.png", bbox_inches='tight')
 plt.close()
-###########
+###################################
+
+
+
+
+# Bootcamp graphs
+#################
+
+
+# 1)
+
+# Bootcamp overall
+values = np.array([4,6,0,0,0])
+labels = np.array(["Excellent", "Very Good", "Good", "Fair", "Poor"])
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.axis("equal")
+obj, text, bla = ax.pie(
+        values,
+        autopct="%1.1f%%",
+        colors=colors
+        )
+
+# remove the zero elements from visible plot
+for elem in bla[2:]:
+  elem.set_visible(False)
+
+ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
+
+fig.savefig("graphs/bootcamp/overall.png", bbox_inches='tight')
+plt.close()
+
+
+# Which part of the university are you from?
+x_list_university = [4, 4, 1]
+label_list_university = ["Department of Economics", "Harris School of Public Policy", "Booth School of Business"]
+plt.axis("equal")
+plt.pie(
+        x_list_university,
+        autopct="%1.1f%%",
+        colors=colors
+        )
+plt.legend(label_list_university, loc='center left', bbox_to_anchor=(0.9, 0.5))
+plt.savefig("graphs/bootcamp/part_univ.png", bbox_inches='tight')
+plt.close()
+
+
+# How well did Philipp meet his goal ...
+
+# to clearly explain the objectives
+values = np.array([7,3,0,0,0])
+labels = np.array(["Excellent", "Very Good", "Good", "Fair", "Poor"])
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.axis("equal")
+obj, text, bla = ax.pie(
+        values,
+        autopct="%1.1f%%",
+        colors=colors
+        )
+
+for elem in bla[2:]:
+  elem.set_visible(False)
+
+ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
+
+fig.savefig("graphs/bootcamp/objectives.png", bbox_inches='tight')
+plt.close()
+
+# to be well organized
+values = np.array([10,0,0,0,0])
+labels = np.array(["Excellent", "Very Good", "Good", "Fair", "Poor"])
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.axis("equal")
+obj, text, bla = ax.pie(
+        values,
+        autopct="%1.1f%%",
+        colors=colors
+        )
+
+for elem in bla[1:]:
+  elem.set_visible(False)
+
+ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
+
+fig.savefig("graphs/bootcamp/organized.png", bbox_inches='tight')
+plt.close()
+
+
+# present at right speed
+values = np.array([6,4,0,0,0])
+labels = np.array(["Excellent", "Very Good", "Good", "Fair", "Poor"])
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.axis("equal")
+obj, text, bla = ax.pie(
+        values,
+        autopct="%1.1f%%",
+        colors=colors
+        )
+
+for elem in bla[2:]:
+  elem.set_visible(False)
+
+ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
+
+fig.savefig("graphs/bootcamp/speed.png", bbox_inches='tight')
+plt.close()
+
+
+# present clearly and understandably
+values = np.array([7,3,0,0,0])
+labels = np.array(["Excellent", "Very Good", "Good", "Fair", "Poor"])
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.axis("equal")
+obj, text, bla = ax.pie(
+        values,
+        autopct="%1.1f%%",
+        colors=colors
+        )
+
+for elem in bla[2:]:
+  elem.set_visible(False)
+
+ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
+
+fig.savefig("graphs/bootcamp/clearly.png", bbox_inches='tight')
+plt.close()
+
+
+# to be accessible
+values = np.array([10,0,0,0,0])
+labels = np.array(["Excellent", "Very Good", "Good", "Fair", "Poor"])
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.axis("equal")
+obj, text, bla = ax.pie(
+        values,
+        autopct="%1.1f%%",
+        colors=colors
+        )
+
+for elem in bla[1:]:
+  elem.set_visible(False)
+
+ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
+
+fig.savefig("graphs/bootcamp/access.png", bbox_inches='tight')
+plt.close()
