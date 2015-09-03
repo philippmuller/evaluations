6dee490c1e3c9fc5d4642ec61222e1e0662a6eed
Philipp Müller <mailphilippmuller@gmail.com>
2015-09-03 15:28:35 +0200

    modified graphs: keep all legend entries when incurring null sets and remove 0.0% labels. adjusted latex notes accordingly.

--- graphs/make_graphs.py
+++ graphs/make_graphs.py
@@ -10,22 +10,32 @@ cmap = plt.cm.prism
 colors = cmap(np.linspace(0., 1., len(slices)))
 
 # Overall Course Rating
-x_list_overall = [9, 2]
-label_list_overall = ["Excellent", "Very Good", "Good", "Fair", "Poor"]
-plt.axis("equal")
-plt.pie(
-        x_list_overall,
+values = np.array([9, 2, 0, 0, 0])
+labels = np.array(["Excellent", "Very Good", "Good", "Fair", "Poor"])
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
+ax.axis("equal")
+obj, text, bla = ax.pie(
+        values,
         autopct="%1.1f%%",
         colors=colors
         )
 
-plt.legend(label_list_overall, loc='center left', bbox_to_anchor=(0.9, 0.5))
-plt.savefig("graphs/overall.png", bbox_inches='tight')
+# remove the zero elements from visible plot
+for elem in bla[2:]:
+  elem.set_visible(False)
+
+ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
+
+fig.savefig("graphs/overall.png", bbox_inches='tight')
 plt.close()
 ###########
 
 
 # University Part
+# !!!!!!!!!!!!!!!!!!!!!!!!Check ordering
 x_list_university = [3, 4, 2, 1]
 label_list_university = ["Department of Economics", "Harris School of Public Policy", "Booth School of Business", "Other"]
 plt.axis("equal")
@@ -42,76 +52,119 @@ plt.close()
 
 
 # Usefulness
-x_list_rating_overall = [1, 2, 7]
-label_list_rating_overall = ["3", "4", "Very useful: 5"]
-plt.axis("equal")
-plt.pie(
-        x_list_rating_overall,
+values = np.array([0, 0, 1, 2, 7])
+labels = np.array(["Not useful at all: 1", "2", "3", "4", "Very useful: 5"])
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
+ax.axis("equal")
+obj, text, bla = ax.pie(
+        values,
         autopct="%1.1f%%",
         colors=colors
         )
 
-plt.legend(label_list_rating_overall, loc='center left', bbox_to_anchor=(0.9, 0.5))
-plt.savefig("graphs/useful.png", bbox_inches='tight')
+# remove the zero elements from visible plot
+for elem in bla[:2]:
+  elem.set_visible(False)
+
+ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
+
+fig.savefig("graphs/useful.png", bbox_inches='tight')
 plt.close()
 ###########
 
 # Well organized?
-x_list_organized = [9,1]
-label_list_organized = ["Excellent", "Very Good", "Good", "Fair", "Poor"]
-plt.axis("equal")
-plt.pie(
-        x_list_organized,
+values = np.array([9, 1, 0, 0, 0])
+labels = np.array(["Excellent", "Very Good", "Good", "Fair", "Poor"])
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
+ax.axis("equal")
+obj, text, bla = ax.pie(
+        values,
         autopct="%1.1f%%",
         colors=colors
         )
 
-plt.legend(label_list_organized, loc='center left', bbox_to_anchor=(0.9, 0.5))
-plt.savefig("graphs/organized.png", bbox_inches='tight')
+# remove the zero elements from visible plot
+for elem in bla[2:]:
+  elem.set_visible(False)
+
+ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
+
+fig.savefig("graphs/organized.png", bbox_inches='tight')
 plt.close()
 ###########
 
 # Right Speed
-x_list_speed = [8,2]
-label_list_speed = ["Excellent", "Very Good", "Good", "Fair", "Poor"]
-plt.axis("equal")
-plt.pie(
-        x_list_speed,
+values = np.array([8,2,0,0,0])
+labels = np.array(["Excellent", "Very Good", "Good", "Fair", "Poor"])
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.axis("equal")
+obj, text, bla = ax.pie(
+        values,
         autopct="%1.1f%%",
         colors=colors
         )
 
-plt.legend(label_list_speed, loc='center left', bbox_to_anchor=(0.9, 0.5))
-plt.savefig("graphs/speed.png", bbox_inches='tight')
+# remove the zero elements from visible plot
+for elem in bla[2:]:
+  elem.set_visible(False)
+
+ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
+
+fig.savefig("graphs/speed.png", bbox_inches='tight')
 plt.close()
 ###########
 
 # clearly
-x_list_clearly = [10]
-label_list_clearly = ["Excellent", "Very Good", "Good", "Fair", "Poor"]
-plt.axis("equal")
-plt.pie(
-        x_list_clearly,
+values = np.array([10,0,0,0,0])
+labels = np.array(["Excellent", "Very Good", "Good", "Fair", "Poor"])
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.axis("equal")
+obj, text, bla = ax.pie(
+        values,
         autopct="%1.1f%%",
         colors=colors
         )
 
-plt.legend(label_list_clearly, loc='center left', bbox_to_anchor=(0.9, 0.5))
-plt.savefig("graphs/clearly.png", bbox_inches='tight')
+# remove the zero elements from visible plot
+for elem in bla[1:]:
+  elem.set_visible(False)
+
+ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
+
+fig.savefig("graphs/clearly.png", bbox_inches='tight')
 plt.close()
 ###########
 
+
+
 # accessible
-x_list_access = [9,1]
-label_list_access = ["Excellent", "Very Good"]
-plt.axis("equal")
-plt.pie(
-        x_list_access,
+values = np.array([9,1,0,0,0])
+labels = np.array(["Excellent", "Very Good", "Good", "Fair", "Poor"])
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.axis("equal")
+obj, text, bla = ax.pie(
+        values,
         autopct="%1.1f%%",
         colors=colors
         )
 
-plt.legend(label_list_access, loc='center left', bbox_to_anchor=(0.9, 0.5))
-plt.savefig("graphs/access.png", bbox_inches='tight')
+# remove the zero elements from visible plot
+for elem in bla[2:]:
+  elem.set_visible(False)
+
+ax.legend(labels=labels, loc='center left', bbox_to_anchor=(0.9, 0.5))
+
+fig.savefig("graphs/access.png", bbox_inches='tight')
 plt.close()
 ###########
